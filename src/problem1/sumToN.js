1963e3c089d163ddc13e6a4adce4ca47ad9c84fa
mister T <aj.tinaes@icloud.com>
2023-06-12 12:01:44 +0800

    tidy up

--- src/problem1/sumToN.js
+++ src/problem1/sumToN.js
@@ -1,11 +1,3 @@
-/*
-Task
-Provide 3 unique implementations of the following function.
-**Input**: `n` - any integer 
-*Assuming this input will always produce a result lesser than `Number.MAX_SAFE_INTEGER`*.
-**Output**: `return` - summation to `n`, i.e. `sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15`.
-*/
-
 // Assuming n >= 0, integer:
 const sum_to_n_a = (n) => (n * (n + 1)) / 2;
 
